986a9e875fb9492e2d5b1079a1de49acb062f0f2
Nick Macon <nickmacon@gmail.com>
2019-12-05 20:29:56 -0600

    12.5 push

--- liri.js
+++ liri.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-var axios = require("axios")
+var axios = require("axios");
 
 var command = process.argv[2];
 var value = process.argv[3];
+var artistString = process.argv.slice(3);
+var artist = artistString.join(" ");
 
 switch(command) {
     case 'movie-this':
@@ -12,11 +14,86 @@ switch(command) {
     break;
 }
 
+switch(command) {
+    case 'concert-this':
+        concert();
+    break;
+}
+
+switch(command) {
+    case 'do-what-it-says':
+        doWhat();
+    break;
+}
+
 function movie() {
     console.log("You are in movie command");
+
+  if (value === undefined) {
+    var queryUrl = "https://www.omdbapi.com/?t=mr+nobody&apikey=trilogy";
+    axios.get(queryUrl)
+    .then(function(response) {
+        console.log("----------------------------------------");
+        console.log("Title: " + response.data.Title);
+        console.log("Year: " + response.data.Year);
+        console.log("IMDB Rating: " + response.data.imdbRating);
+        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        console.log("Country produced in: " + response.data.Country);
+        console.log("Language: " + response.data.Language);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Actors: " + response.data.Actors);
+        console.log("If you haven't watched 'Mr. Nobody' then you should: <http://www.imdb.com/title/tt0485947/>");
+        console.log("It's on Netflix!");
+        console.log("----------------------------------------");
+  });
+  }
+
+  else {
     var queryUrl = "https://www.omdbapi.com/?t=" + value + "&apikey=trilogy";
     axios.get(queryUrl)
-    .then(function(data) {
-        console.log(data);
+    .then(function(response) {
+        console.log("----------------------------------------");
+        console.log("Title: " + response.data.Title);
+        console.log("Year: " + response.data.Year);
+        console.log("IMDB Rating: " + response.data.imdbRating);
+        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+        console.log("Country produced in: " + response.data.Country);
+        console.log("Language: " + response.data.Language);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Actors: " + response.data.Actors);
+        console.log("----------------------------------------");
     });
+  }
+}
+
+function concert() {
+    console.log("You are in concert command");
+
+  if (value === undefined) {
+    var queryUrl = "https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp";
+    axios.get(queryUrl)
+    .then(function(response) {
+        for (var i=0; i < response.data.length; i++) {
+        console.log("----------------------------------------");
+        console.log("Venue Name: " + response.data[i].venue.name);
+        console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+        console.log("Concert Date: " + response.data[i].datetime);
+        console.log("----------------------------------------");
+        }
+    });
+  }
+
+  else {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(queryUrl)
+        .then(function(response) {
+    for (var i=0; i < response.data.length; i++) {
+        console.log("----------------------------------------");
+        console.log("Venue Name: " + response.data[i].venue.name);
+        console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+        console.log("Concert Date: " + response.data[i].datetime);
+        console.log("----------------------------------------");
+        }
+    });
+  }
 }
