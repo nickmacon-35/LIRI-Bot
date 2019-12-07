e871d44e31782a69a536a0f781fca3b5743a1933
Nick Macon <nickmacon@gmail.com>
2019-12-07 17:04:34 -0600

    12.7 push

--- liri.js
+++ liri.js
@@ -3,45 +3,36 @@ require("dotenv").config();
 var fs = require("fs");
 var moment = require("moment");
 var keys = require("./keys.js");
-// console.log(keys.id);
-// console.log(keys.secret);
 var axios = require("axios");
 
-
 var command = process.argv[2];
-var value = process.argv[3];
-var movie = process.argv.slice(3).join(" ");
-var artist = process.argv.slice(3).join(" ");
-var song = process.argv.slice(3).join(" ");
+var value = process.argv.slice(3).join(" ");
 
-switch(command) {
+function func(comm, val) {
+switch(comm) {
     case 'movie-this':
-        movie();
+        movie(val);
     break;
-}
 
-switch(command) {
     case 'concert-this':
-        concert();
+        concert(val);
     break;
-}
 
-switch(command) {
     case 'spotify-this-song':
-        spotify();
+        spotify(val);
     break;
+
+    case 'do-what-it-says':
+        doWhat(val);
+    break;
+}
 }
 
-switch(command) {
-    case 'do-what-it-says':
-        doWhat();
-    break;
-}
 
 function movie() {
     console.log("You are in movie command");
 
-  if (value === undefined) {
+  if (!process.argv[3]) {
     var queryUrl = "https://www.omdbapi.com/?t=mr+nobody&apikey=trilogy";
     axios.get(queryUrl)
     .then(function(response) {
@@ -61,7 +52,7 @@ function movie() {
   }
 
   else {
-    var queryUrl = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+    var queryUrl = "https://www.omdbapi.com/?t=" + value + "&apikey=trilogy";
     axios.get(queryUrl)
     .then(function(response) {
         console.log("----------------------------------------");
@@ -81,7 +72,7 @@ function movie() {
 function concert() {
     console.log("You are in concert command");
 
-  if (value === undefined) {
+  if (!process.argv[3]) {
     var queryUrl = "https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp";
     axios.get(queryUrl)
     .then(function(response) {
@@ -97,7 +88,7 @@ function concert() {
   }
 
   else {
-    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    var queryUrl = "https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp";
     axios.get(queryUrl)
         .then(function(response) {
     for (var i=0; i < response.data.length; i++) {
@@ -115,14 +106,14 @@ function concert() {
 function spotify() {
     console.log("You are in spotify command")
 
-  if (value === undefined) {
+  if (!process.argv[3]) {
     keys.search({ type: 'track', query: "The Sign" })
         .then(function(response) {
         console.log("----------------------------------------");
-        console.log("Artist: " + JSON.stringify(response.tracks.items[6].album.artists[0].name));
-        console.log("Song Title: " + JSON.stringify(response.tracks.items[6].name));
-        console.log("Spotify Link to the song: " + JSON.stringify(response.tracks.items[6].external_urls.spotify));
-        console.log("From Album: " + JSON.stringify(response.tracks.items[6].album.name));
+        console.log("Artist: " + response.tracks.items[6].album.artists[0].name);
+        console.log("Song Title: " + response.tracks.items[6].name);
+        console.log("Spotify Link to the song: " + response.tracks.items[6].external_urls.spotify);
+        console.log("From Album: " + response.tracks.items[6].album.name);
         console.log("----------------------------------------");
     })
     .catch(function(err) {
@@ -131,14 +122,14 @@ function spotify() {
   }
 
   else {
-    keys.search({ type: 'track', query: song })
+    keys.search({ type: 'track', query: value })
         .then(function(response) {
-    for (var i=0; i < response.tracks.items[i].length; i++) {
+    for (var i=0; i < JSON.stringify(response.tracks.items[i]).length; i++) {
         console.log("----------------------------------------");
-        console.log("Artist: " + JSON.stringify(response.tracks.items[i].album.artists[0].name));
-        console.log("Song Title: " + JSON.stringify(response.tracks.items[i].name));
-        console.log("Spotify Link to the song: " + JSON.stringify(response.tracks.items[i].external_urls.spotify));
-        console.log("From Album: " + JSON.stringify(response.tracks.items[i].album.name));
+        console.log("Artist: " + response.tracks.items[i].album.artists[0].name);
+        console.log("Song Title: " + response.tracks.items[i].name);
+        console.log("Spotify Link to the song: " + response.tracks.items[i].external_urls.spotify);
+        console.log("From Album: " + response.tracks.items[i].album.name);
         console.log("----------------------------------------");
     }
     })
@@ -148,13 +139,18 @@ function spotify() {
   }
 }
 
+function doWhat() {
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (err) {
+          return console.log(err);
+        }
+    // Break the string down by comma separation and store the contents into the output array.
+    var output = data.split(",");
+    console.log(output[0]);
+    console.log(output.slice(1).join(" "));
+    value = output.slice(1).join(" ");
+    func(output[0], value);
+  }); 
+}
 
-// function doWhat() {
-//     fs.readFile("random.txt", "utf8", function(err, data) {
-//         if (err) {
-//           return console.log(err);
-//         }
-//     // Break the string down by comma separation and store the contents into the output array.
-//     var output = data.split(",");
-    
-// }
+func(command, value);
